ad0205a78dbfdea0e7df2ce43cce830f43546390
Sławomir Lech <contact.slawomir.lech@gmail.com>
2023-01-09 00:12:10 +0100

    Update pot file

--- bin/make-pot.sh
+++ bin/make-pot.sh
@@ -8,5 +8,5 @@ DEFAULT_TEMPLATE_PATH="$LANGUAGES_PATH/$PLUGIN_SLUG.pot"
 
 echo "Generating default template..."
 mkdir -p "$LANGUAGES_PATH"
-wp i18n make-pot $PROJECT_PATH $DEFAULT_TEMPLATE_PATH --domain=$TEXT_DOMAIN --exclude=$PROJECT_PATH --include=$PROJECT_PATH/src,$PROJECT_PATH/templates
+wp i18n make-pot $PROJECT_PATH $DEFAULT_TEMPLATE_PATH --domain=$TEXT_DOMAIN --exclude=$PROJECT_PATH --include=$PROJECT_PATH/src,$PROJECT_PATH/templates --allow-root
 echo "Done!"
